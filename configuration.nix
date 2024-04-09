362823290aad3dae2e0b0e00185eab169b1954af
mudia <example@example.com>
2024-04-09 05:23:52 +0100

    remove kde

--- configuration.nix
+++ configuration.nix
@@ -54,7 +54,6 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
